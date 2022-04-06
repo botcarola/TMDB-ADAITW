9638428f1446de7832343892ea5db9fb02bb63dc
Elizabeth Carolina <celerner4@gmail.com>
2022-04-06 13:06:41 -0300

    agrego ruteo en App.js, creo componente home, instalo react router, instalo tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from "./components/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  return(
+    <>
+    <BrowserRouter>
+    <h2>Hola:D</h2>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/movies" element={<Home />} />
+              <Route path="/movie/:id/:info" element={<Home />} />
+
+              <Route path="/movies/trending/page/:pagina" element={<Home />} />
+              <Route path="/movies/popular/page/:pagina" element={<Home />} />
+              <Route path="/movies/top_rated/page/:pagina" element={<Home />} />
+              <Route path="/movies/upcoming/page/:pagina" element={<Home />} />
+              <Route path="/movies/now_playing/page/:pagina" element={<Home />} />           
+
+              <Route path="/tv" element={<Home />} />
+              <Route path="/tv/:id/:info" element={<Home />} />  
+
+              <Route path="/tv/trending/page/:pagina" element={<Home/>} />
+              <Route path="/tv/popular/page/:pagina" element={<Home/>} />
+              <Route path="/tv/top_rated/page/:pagina" element={<Home />} />
+              <Route path="/tv/on_the_air/page/:pagina" element={<Home />} />
+
+              <Route path="/*" element={<Home />} />
+            </Routes>
+    </BrowserRouter>
+    </>
+  )
 }
 
 export default App;
