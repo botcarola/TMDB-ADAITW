0c906689f1ceddf82f5891e325065d4323ef31bd
Elizabeth Carolina <celerner4@gmail.com>
2022-04-10 14:47:33 -0300

    termino de desarrollar componente error

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,10 +1,10 @@
+import error from "../assets/error.png";
+
 const Error = () => {
 
     return(
-        <div className="flex justify-center ">
-
-
-                 
+        <div className="flex justify-center">
+            <img className="mt-11 mb-7" src={error} alt="Error" />                 
         </div>
     )
 }
